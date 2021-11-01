b3323dd6688248eaa99fa56235c0d2e2baf8b685
Miguel Costa <mig.reis@ua.pt>
2021-11-01 11:42:40 +0000

    New App Created With Bottom Navigation Bar.

--- tripfinder/lib/main.dart
+++ tripfinder/lib/main.dart
@@ -1,167 +1,47 @@
-import 'dart:async';
+/// Flutter code sample for BottomNavigationBar
+
+// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
+// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
+// widgets, which means it defaults to [BottomNavigationBarType.fixed], and
+// the [currentIndex] is set to index 0. The selected item is
+// amber. The `_onItemTapped` function changes the selected item's index
+// and displays a corresponding message in the center of the [Scaffold].
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MapSample(),
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-          ]
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-/// *******************************************************************
-///  ************************ Map *********************************
-///********************************************************************
-class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
-
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake =  CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}
-
-/// *******************************************************************
-///  ************************ Navbar *********************************
-///********************************************************************
-class NavBar extends StatefulWidget {
-  const NavBar({Key? key}) : super(key: key);
-
-  @override
-  State<NavBar> createState() => _NavBar();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
 /// This is the private State class that goes with MyStatefulWidget.
-class _NavBar extends State<NavBar> {
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    HomePage(title: 'TripFinder'),
-    Text('2nd Panel', style: optionStyle),
-    Text('No new notifications!', style: optionStyle),
-    Text('4th Panel', style: optionStyle),
+    Home(title: 'Home'),
+    Stat(title: 'Stats'),
+    Notification(title: 'Notification'),
+    Profile(title: 'Profile'),
   ];
 
   void _onItemTapped(int index) {
@@ -173,38 +53,156 @@ class _NavBar extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('TripFinder'),
+        backgroundColor: Colors.black,
+      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        backgroundColor: Colors.white,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home,),
+            icon: Icon(Icons.home),
             label: 'Home',
-            backgroundColor: Colors.white,
-          ),
-          
-          BottomNavigationBarItem(
-            icon: Icon(Icons.play_circle,),
-            label: 'Courses',
-            backgroundColor: Colors.white,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.notifications,),
+            icon: Icon(Icons.bar_chart),
+            label: 'Statistics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notification_important_outlined),
             label: 'Notifications',
-            backgroundColor: Colors.white,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person,),
+            icon: Icon(Icons.account_circle),
             label: 'Profile',
-            backgroundColor: Colors.white,
           ),
         ],
         currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black,
         onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Home> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Align(
+          child: Text(
+            'Home Page',
+            style: optionStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Stat extends StatefulWidget {
+  const Stat({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Stat> createState() => _Stat();
+}
+
+class _Stat extends State<Stat> {
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Align(
+          child: Text(
+            'Stat Page',
+            style: optionStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Notification extends StatefulWidget {
+  const Notification({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Notification> createState() => _Notification();
+}
+
+class _Notification extends State<Notification> {
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Align(
+          child: Text(
+            'Notifications',
+            style: optionStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Profile> createState() => _Profile();
+}
+
+class _Profile extends State<Profile> {
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Align(
+          child: Text(
+            'Profile',
+            style: optionStyle,
+          ),
+        ),
       ),
     );
   }
