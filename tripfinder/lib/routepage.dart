1d27a32ef4b8c4e488870715f0f35a1987bfecfa
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2021-11-13 18:16:08 +0000

    camera another try, no errors

--- tripfinder/lib/routepage.dart
+++ tripfinder/lib/routepage.dart
@@ -10,6 +10,8 @@ import 'dart:async';
 import 'package:location/location.dart';
 import 'package:camera/camera.dart';
 
+import 'camerapage.dart';
+
 List<CameraDescription> cameras = [];
 
 final Set<Polyline> route = <Polyline>{
@@ -160,6 +162,7 @@ class MapSampleState extends State<MapSample> {
             getCurrentLocation();
           }
       ),
+      persistentFooterButtons: [TextButton(onPressed: _pictureScreen, child: const Text("Take a Picture!"))],
     );
   }
 
@@ -171,132 +174,14 @@ class MapSampleState extends State<MapSample> {
     cameras = await availableCameras(); 
 
     // Get a specific camera from the list of available cameras.
-    //final firstCamera = cameras.first;
-
-    if (await Permission.camera.request().isGranted){
-      return CameraApp();
-    }
-    else{
-      return const MapSample();
-    }
+    final firstCamera = cameras.first;
+    
+    return TakePictureScreen(camera: firstCamera);
   }
 
 }
 
-/// ************************
-/// ******* Camera *********
-/// ************************
-/*class TakePictureScreen extends StatefulWidget {
-  const TakePictureScreen({
-    Key? key,
-    required this.camera,
-  }) : super(key: key);
-
-  final CameraDescription camera;
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<TakePictureScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('TripFinder')),
-      // You must wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner until the
-      // controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
-
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
-
-            // If the picture was taken, display it on a new screen.
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image.path,
-                ),
-              ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
-    );
-  }
-}
-
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({Key? key, required this.imagePath})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('TripFinder')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
-    );
-  }
-}*/
-
-class CameraApp extends StatefulWidget {
+/*class CameraApp extends StatefulWidget {
   @override
 
   _CameraAppState createState() => _CameraAppState();
@@ -332,4 +217,4 @@ class _CameraAppState extends State<CameraApp> {
       home: CameraPreview(controller),
     );
   }
-}
+}*/
